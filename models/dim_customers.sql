ad9089a1517b5b63f4969cbcdd3a0929eb4a9a6c
Adam Kabak <adamkabak@yahoo.com>
2023-10-15 14:06:37 -0400

    add dim_customers

---
+++ models/dim_customers.sql
@@ -0,0 +1,46 @@
+with customers as (
+    SELECT
+        id as customer_id,
+        first_name,
+        last_name
+    FROM
+        `dbt-tutorial.jaffle_shop.customers`
+),
+
+orders as (
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+    from 
+        `dbt-tutorial.jaffle_shop.orders`
+),
+
+customer_orders as (
+    select
+        customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders,
+
+    from orders
+    group by 1
+),
+
+final as (
+    SELECT
+        customers.custer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        coaslesce(customer_orders.number_of_orders, 0) as number_of_orders
+
+    FROM
+        customers
+    left join
+        customer_orders using (customer_id)
+)
+
+select * from final
