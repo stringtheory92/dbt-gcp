e49b8ffe54f57d771c568736f1ceaf427becc6c7
Adam Kabak <adamkabak@yahoo.com>
2023-10-15 14:09:34 -0400

    typo

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -35,7 +35,7 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coaslesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
     FROM
         customers
