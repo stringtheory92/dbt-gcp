b50324bc32aad851b5d73cdf82717404d9bf1381
Adam Kabak <adamkabak@gmail.com>
2023-10-15 19:22:47 +0000

    optimized queries

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -4,10 +4,18 @@ with orders as (
 
 payments as (
     select * from {{ ref('stg_payments') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+    from payments
+    group by 1
 )
 
 select 
     orders.order_id,
     orders.customer_id,
-    payments.amount
-from orders inner join payments using (order_id)
+    coalesce(order_payments.amount, 0) as amount
+from orders left join order_payments using (order_id)
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -2,7 +2,10 @@ with payments as (
     select 
         id as payment_id,
         orderid as order_id,
-        amount,
+        amount / 100 as amount,
+        created as created_at,
+        status,
+        paymentmethod as payment_method
     from `dbt-tutorial.stripe.payment`
 )
 
